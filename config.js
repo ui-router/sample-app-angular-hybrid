a31a025864349e27f04a59c0ec0108c876868338
Chris Thielen <github@sandgnat.com>
2016-04-03 16:57:55 -0500

    chore(): add angular2, ui-router-ng2, and ui-router-ng1-to-ng2

--- config.js
+++ config.js
@@ -1,24 +1,39 @@
+var packages = {
+  "app": { "defaultExtension": "js" }
+};
+
+var ng2PackageNames = [
+  'common', 'compiler', 'core', 'http', 'platform-browser', 'platform-browser-dynamic', 'testing', 'upgrade'
+];
+
+// add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
+ng2PackageNames.forEach(function(pkgName) {
+  packages["@angular/" + pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+});
+
 System.config({
   transpiler: false,
   defaultJSExtensions: true,
 
-  packages: {
-    "app": { "defaultExtension": "js" }
-  },
+  packages: packages,
 
   paths: { "npm:*": "node_modules/*" },
 
   meta: {
-    "npm:angular/angular.min": { format: "global", exports: "angular" }
+    "npm:angular/angular": { format: "global", exports: "angular" }
   },
 
   map: {
-    "angular": "npm:angular/angular.min",
+    "angular": "npm:angular/angular",
+    "@angular": "npm:@angular",
+    "rxjs": "npm:rxjs",
     "angular-ui-router": "npm:angular-ui-router/release/angular-ui-router",
+    "ui-router-ng2": "npm:ui-router-ng2/_bundles/ui-router-ng2.js",
     "bootstrap": "npm:bootstrap/dist",
     "css": "npm:systemjs-plugin-css/css",
     "d3": "npm:d3/d3.min",
     "font-awesome": "npm:font-awesome",
+    "ui-router-ng1-to-ng2": "npm:ui-router-ng1-to-ng2/ng1-to-ng2",
     "ui-router-visualizer": "npm:ui-router-visualizer/release/visualizer.min"
   }
 });
