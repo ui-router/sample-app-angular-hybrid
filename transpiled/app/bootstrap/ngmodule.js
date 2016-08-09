e67a1b3c52601147b58c33928ae4de0a6fed421b
Chris Thielen <github@sandgnat.com>
2016-08-08 20:54:03 -0500

    commit transpiled code

--- transpiled/app/bootstrap/ngmodule.js
+++ transpiled/app/bootstrap/ngmodule.js
@@ -7,11 +7,18 @@
 var angular = require("angular");
 var angular_ui_router_1 = require("angular-ui-router");
 var ui_router_visualizer_1 = require("ui-router-visualizer");
-// Create the angular module "demo".
+// Create the angular 1 module "demo".
 //
 // Since it is exported, other parts of the application (in other files) can then import it and register things.
 // In bootstrap.js, the module is imported, and the components, services, and states are registered.
-exports.ngmodule = angular.module("demo", [angular_ui_router_1.default]);
+exports.ngmodule = angular.module("demo", [angular_ui_router_1.default, 'ui.router.upgrade']);
 // Show ui-router-visualizer
-exports.ngmodule.run(function (ng1UIRouter) { return ui_router_visualizer_1.visualizer(ng1UIRouter); });
+exports.ngmodule.run(['$injector', function ($injector) {
+        if ($injector.has('ng1UIRouter')) {
+            ui_router_visualizer_1.visualizer($injector.get('ng1UIRouter'));
+        }
+        else if ($injector.has('$uiRouter')) {
+            ui_router_visualizer_1.visualizer($injector.get('$uiRouter'));
+        }
+    }]);
 //# sourceMappingURL=ngmodule.js.map
