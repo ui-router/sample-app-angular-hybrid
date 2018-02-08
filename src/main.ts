ae837eab75c7628e51c2adc53faf85cc5f6b2c53
Chris Thielen <cthielen@netflix.com>
2018-02-07 20:50:58 -0800

    Remove angular hybrid

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
-import { UpgradeModule } from '@angular/upgrade/static';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Component, Inject, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { SampleAppModuleAngular } from './AngularModule';
-import { sampleAppModuleAngularJS } from "./AngularJSModule";
+export class SomeClass {
+  title = "This is an instance of SomeClass";
+  toString() {
+    return this.title
+  }
+}
 
-// Manually bootstrap the Angular app
-platformBrowserDynamic().bootstrapModule(SampleAppModuleAngular).then(platformRef => {
-  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
-  upgrade.bootstrap(document.body, [sampleAppModuleAngularJS.name]);
-});
+@Component({
+  selector: 'angular-cmp',
+  template: `<h1>Angular component</h1>`,
+})
+export class AngularCmp {
+  constructor(
+      @Inject(SomeClass) public someObject: SomeClass, // this works
+      // public someObject: SomeClass, // this does not work
+  ) {
+    console.log("Object:", this.someObject)
+  }
+}
+
+@NgModule({
+  imports: [ BrowserModule ],
+  providers: [
+    { provide: SomeClass, useClass: SomeClass },
+  ],
+  entryComponents: [ AngularCmp ],
+  declarations: [ AngularCmp ],
+  bootstrap: [ AngularCmp ],
+})
+export class SampleAppModuleAngular {
+}
+
+platformBrowserDynamic().bootstrapModule(SampleAppModuleAngular);
