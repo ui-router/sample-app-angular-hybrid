8acf5aa7e752a510a996d930c3bd7d59bd97d9dd
Chris Thielen <cthielen@netflix.com>
2018-02-07 20:40:53 -0800

    Remove ui-router references

--- src/AngularJSModule.ts
+++ src/AngularJSModule.ts
@@ -1,15 +1,14 @@
 import * as angular from "angular";
-import { upgradeModule } from "@uirouter/angular-hybrid";
 
-import { AngularCmp } from './angularModule';
+import { AngularCmp } from './AngularModule';
 import { downgradeComponent } from '@angular/upgrade/static';
 
 const appComponent = {
   template: `
-    <h1>app component</h1>
+    <h1>AngularJS app component</h1>
     <downgraded-angular-cmp></downgraded-angular-cmp>
 `};
 
-export const sampleAppModuleAngularJS = angular.module("sampleapp", [ upgradeModule.name ])
+export const sampleAppModuleAngularJS = angular.module("sampleapp", [ ])
   .component('app', appComponent)
   .directive('downgradedAngularCmp', downgradeComponent({ component: AngularCmp }));
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-////////////// HYBRID BOOTSTRAP ///////////////
-import { Injector } from '@angular/core';
 import { UpgradeModule } from '@angular/upgrade/static';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { SampleAppModuleAngular } from './angularModule';
-import { sampleAppModuleAngularJS } from "./angularJSModule";
+import { SampleAppModuleAngular } from './AngularModule';
+import { sampleAppModuleAngularJS } from "./AngularJSModule";
 
 // Manually bootstrap the Angular app
 platformBrowserDynamic().bootstrapModule(SampleAppModuleAngular).then(platformRef => {
-  const injector: Injector = platformRef.injector;
-  const upgrade = injector.get(UpgradeModule) as UpgradeModule;
-
-  // The DOM must be already be available
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
   upgrade.bootstrap(document.body, [sampleAppModuleAngularJS.name]);
 });
